9ddef6c8d18457c6c6d25d6ad0dab945c6495ad1
Akbota <akbota.zhu@gmail.com>
2020-09-15 11:12:28 +0600

    New file added

---
+++ seminar2.py
@@ -0,0 +1,10 @@
+text_input = input("Enter a text: ")
+text = text_input.lowercase()
+alphabet = "abcdefghijklmnopqrstyvwxyz"
+key = "qwertyuiopasdfghjklzxcvbnm"
+
+def encode(text):
+    encoded_text = ""
+
+    return encoded_text
+
